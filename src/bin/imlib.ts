451ef4d95cda73abd4516ca45cdc580a3ac3b5a2
sdegutis <sbdegutis@gmail.com>
2024-08-02 15:16:33 -0500

    Clean up.

--- src/bin/imlib.ts
+++ src/bin/imlib.ts
@@ -3,13 +3,12 @@
 import { startDevServer } from "./dev";
 import { generateFiles } from "./generate";
 
-const cmd = process.argv[2] ?? '';
-
 const fns: Record<string, () => void> = {
   dev: startDevServer,
   generate: generateFiles,
 };
 
+const cmd = process.argv[2] ?? '';
 const fn = fns[cmd] ?? showHelp;
 fn();
 
