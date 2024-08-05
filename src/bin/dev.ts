f31f19063877f714ebc67686544c39e3371a7dd5
sdegutis <sbdegutis@gmail.com>
2024-08-05 13:03:26 -0500

    Inline single-use dev server.

--- src/bin/dev.ts
+++ src/bin/dev.ts
@@ -1,7 +1,7 @@
 import { Runtime, SiteProcessor } from '@imlib/runtime';
 import * as chokidar from 'chokidar';
+import * as http from "http";
 import * as path from 'path';
-import { Server } from '../server.js';
 
 export function startDevServer(config: {
   siteDir: string;
@@ -50,4 +50,60 @@ export function startDevServer(config: {
     .on('add', pathUpdated)
     .on('change', pathUpdated)
     .on('unlink', pathUpdated));
+}
+
+class Server {
+
+  files: Map<string, Buffer | string> | undefined;
+  handlers?: Map<string, (body: string) => string> | undefined;
+
+  rebuilt = () => { };
+
+  startServer(port: number) {
+    const server = http.createServer((req, res) => {
+      const url = req.url!.split('?')[0]!;
+
+      if (req.method === 'POST') {
+        const handler = this.handlers?.get(url);
+        if (handler) {
+          const data: Buffer[] = [];
+          req.on('data', (chunk) => {
+            data.push(chunk);
+          });
+          req.on('end', () => {
+            let redirect: string;
+            this.rebuilt = () => {
+              res.statusCode = 302;
+              res.setHeader('Location', redirect);
+              res.end();
+            };
+            const body = Buffer.concat(data).toString('utf8');
+            redirect = handler(body);
+          });
+        }
+        return;
+      }
+
+      const content = (
+        this.files?.get(url) ??
+        this.files?.get(path.join(url, 'index.html'))
+      );
+
+      if (content !== undefined) {
+        res.statusCode = 200;
+        if (url.endsWith('.js')) {
+          res.setHeader('content-type', 'text/javascript');
+        }
+        res.end(content);
+      }
+      else {
+        res.statusCode = 404;
+        res.end('File not found');
+      }
+    });
+
+    server.listen(port);
+    console.log(`Running on http://localhost:${port}`);
+  }
+
 }
