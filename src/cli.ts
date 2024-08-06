553637d56fe2019d4074bd9f25d13ebe1fcc95a3
sdegutis <sbdegutis@gmail.com>
2024-08-06 08:56:12 -0500

    Shorter/simpler.

--- src/cli.ts
+++ src/cli.ts
@@ -4,17 +4,12 @@ import { startDevServer } from './dev-server';
 import { generateFiles } from './file-generator';
 import { Runtime } from './runtime';
 
-const runtime = new Runtime();
-
-const fns: Record<string, () => void> = {
-  dev: () => startDevServer(runtime),
-  generate: () => generateFiles(runtime),
+const fns = {
+  dev: () => startDevServer(new Runtime()),
+  generate: () => generateFiles(new Runtime()),
+  help: () => console.log("Usage: imlib <dev | generate>"),
 };
 
-const cmd = process.argv[2] ?? '';
-const fn = fns[cmd] ?? showHelp;
-fn();
-
-function showHelp() {
-  console.log("Usage: imlib <dev | generate>");
-}
+const usrcmd = (process.argv[2] ?? '') as keyof typeof fns;
+const cmd = usrcmd in fns ? usrcmd : 'help';
+fns[cmd]();
