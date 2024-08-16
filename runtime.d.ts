8c7df97b6ca3a59684d2fd79b6a0b20af464fda5
sdegutis <sbdegutis@gmail.com>
2024-08-16 05:24:27 -0500

    Adding some types.

--- runtime.d.ts
+++ runtime.d.ts
@@ -1,6 +1,38 @@
 declare module JSX {
-  export type IntrinsicElements = {
+  export type ElementAttrs = {
+    id?: string;
+    class?: string;
+    style?: string;
+    title?: string;
+    innerHTML?: string;
+    hidden?: boolean;
+  };
+
+  export type AnchorAttrs = ElementAttrs & { href?: string; rel?: 'noopener'; target?: string; onclick?: string | Function; };
+  export type MetaAttrs = { 'http-equiv'?: string; content?: string; name?: string; charset?: string; property?: string; };
+  export type LinkAttrs = { href: string } & (
+    { rel: 'stylesheet' } |
+    { rel: 'icon'; type: string; sizes: string } |
+    { rel: 'apple-touch-icon'; sizes: string } |
+    { rel: 'preload'; as: 'font'; type: 'font/woff'; crossorigin: boolean } |
+    { rel: 'manifest' });
+  export type ScriptAttrs = ElementAttrs & { type?: 'module'; src?: string };
+
+  type IntrinsicElements = {
     [tag: string]: Record<string, string | boolean | Function>;
+    meta: MetaAttrs,
+    link: LinkAttrs,
+    a: AnchorAttrs,
+    b: ElementAttrs,
+    div: ElementAttrs,
+    h1: ElementAttrs,
+    h2: ElementAttrs,
+    h3: ElementAttrs,
+    h4: ElementAttrs,
+    h5: ElementAttrs,
+    h6: ElementAttrs,
+    p: ElementAttrs,
+    script: ScriptAttrs,
   };
   export type Element = HTMLElement | SVGElement | DocumentFragment | string;
   export type Component<T extends Record<string, any> = {}> =
