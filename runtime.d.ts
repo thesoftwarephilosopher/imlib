500d308839d6250aafa1eb67f7641a64f60cebe1
sdegutis <sbdegutis@gmail.com>
2024-08-18 11:47:44 -0500

    Make all attributes optional.

--- runtime.d.ts
+++ runtime.d.ts
@@ -13,24 +13,24 @@ declare module JSX {
   export type HtmlAttrs = { lang?: string };
   export type AnchorAttrs = ElementAttrs & { href?: string; rel?: 'noopener'; target?: string };
   export type MetaAttrs = { 'http-equiv'?: string; content?: string; name?: string; charset?: string; property?: string; };
-  export type LinkAttrs = { href: string } & (
-    { rel: 'stylesheet' } |
-    { rel: 'icon'; type: string; sizes: string } |
-    { rel: 'apple-touch-icon'; sizes: string } |
-    { rel: 'preload'; as: 'font'; type: 'font/woff'; crossorigin: boolean } |
-    { rel: 'manifest' });
+  export type LinkAttrs = { href?: string } & (
+    { rel?: 'stylesheet' } |
+    { rel?: 'icon'; type?: string; sizes?: string } |
+    { rel?: 'apple-touch-icon'; sizes?: string } |
+    { rel?: 'preload'; as?: 'font'; type?: 'font/woff'; crossorigin?: boolean } |
+    { rel?: 'manifest' });
   export type ScriptAttrs = ElementAttrs & { type?: 'module'; src?: string };
-  export type ImgAttrs = ElementAttrs & { src: string; loading?: 'lazy', alt?: '' };
+  export type ImgAttrs = ElementAttrs & { src?: string; loading?: 'lazy', alt?: '' };
   export type FormAttrs = ElementAttrs & { method?: string; action?: string };
   export type ButtonAttrs = ElementAttrs & { type?: string };
   export type InputAttrs = ElementAttrs & { type?: string; name?: string; value?: string; checked?: boolean; autofocus?: boolean; placeholder?: string; oninput?: string | Function; autocomplete?: string };
   export type TextAreaAttrs = ElementAttrs & { name?: string; rows?: string };
   export type SelectAttrs = ElementAttrs & { name?: string; onchange?: string | Function };
   export type OptionAttrs = ElementAttrs & { value?: string; selected?: boolean };
-  export type OptgroupAttrs = ElementAttrs & { label: string };
-  export type IFrameAttrs = ElementAttrs & { src: string; allowfullscreen?: boolean | 'allowfullscreen' | ''; width?: string; height?: string; frameborder?: string; loading?: 'lazy'; allow?: string };
-  export type SvgAttrs = ElementAttrs & { viewBox: string; height?: string };
-  export type PathAttrs = ElementAttrs & { d: string };
+  export type OptgroupAttrs = ElementAttrs & { label?: string };
+  export type IFrameAttrs = ElementAttrs & { src?: string; allowfullscreen?: boolean | 'allowfullscreen' | ''; width?: string; height?: string; frameborder?: string; loading?: 'lazy'; allow?: string };
+  export type SvgAttrs = ElementAttrs & { viewBox?: string; height?: string };
+  export type PathAttrs = ElementAttrs & { d?: string };
 
   type IntrinsicElements = {
     [tag: string]: Record<string, string | boolean | Function>;
