7dca04b8349f160fa328ebc5cb7760fc49b44473
sdegutis <sbdegutis@gmail.com>
2024-08-18 11:53:10 -0500

    Making JSX a little more convenient.

--- runtime.d.ts
+++ runtime.d.ts
@@ -1,13 +1,28 @@
 declare module JSX {
   export type ElementAttrs = {
-    [tag: string]: string | boolean | Function;
+    [tag: string]: string | boolean | ((e: Event) => any);
     id?: string;
     class?: string;
     style?: string;
     title?: string;
     innerHTML?: string;
     hidden?: boolean;
-    onclick?: string | Function,
+
+    onclick?: string | ((e: MouseEvent) => any),
+    onmousedown?: string | ((e: MouseEvent) => any),
+    onmouseenter?: string | ((e: MouseEvent) => any),
+    onmouseleave?: string | ((e: MouseEvent) => any),
+    onmousemove?: string | ((e: MouseEvent) => any),
+    onmouseover?: string | ((e: MouseEvent) => any),
+    onmouseup?: string | ((e: MouseEvent) => any),
+
+    oninput?: string | ((e: Event) => any),
+    onchange?: string | ((e: Event) => any),
+
+    onkeydown?: string | ((e: KeyboardEvent) => any),
+    onkeyup?: string | ((e: KeyboardEvent) => any),
+
+    onload?: string | ((e: Event) => any),
   };
 
   export type HtmlAttrs = { lang?: string };
@@ -23,9 +38,9 @@ declare module JSX {
   export type ImgAttrs = ElementAttrs & { src?: string; loading?: 'lazy', alt?: '' };
   export type FormAttrs = ElementAttrs & { method?: string; action?: string };
   export type ButtonAttrs = ElementAttrs & { type?: string };
-  export type InputAttrs = ElementAttrs & { type?: string; name?: string; value?: string; checked?: boolean; autofocus?: boolean; placeholder?: string; oninput?: string | Function; autocomplete?: string };
+  export type InputAttrs = ElementAttrs & { type?: string; name?: string; value?: string; checked?: boolean; autofocus?: boolean; placeholder?: string; autocomplete?: string };
   export type TextAreaAttrs = ElementAttrs & { name?: string; rows?: string };
-  export type SelectAttrs = ElementAttrs & { name?: string; onchange?: string | Function };
+  export type SelectAttrs = ElementAttrs & { name?: string };
   export type OptionAttrs = ElementAttrs & { value?: string; selected?: boolean };
   export type OptgroupAttrs = ElementAttrs & { label?: string };
   export type IFrameAttrs = ElementAttrs & { src?: string; allowfullscreen?: boolean | 'allowfullscreen' | ''; width?: string; height?: string; frameborder?: string; loading?: 'lazy'; allow?: string };
