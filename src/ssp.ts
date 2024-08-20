8fd1aae7ff71deb1c26a3d264216d604161975cc
sdegutis <sbdegutis@gmail.com>
2024-08-20 08:42:32 -0500

    Clean up site processor logic.

--- src/ssp.ts
+++ src/ssp.ts
@@ -1,8 +1,9 @@
+import * as path from "path";
 import { File } from "./file.js";
 
-const postProcessors: Record<string, PostProcessor> = {
-  html: hoistHtml,
-  json: JSON.stringify,
+export const contentProcessors: Record<string, (s: any) => string> = {
+  '.html': hoistHtml,
+  '.json': JSON.stringify,
 };
 
 function hoistHtml(jsx: string) {
@@ -15,45 +16,13 @@ function hoistHtml(jsx: string) {
     .replace(/<\/head>/, [...hoisted, '</head>'].join('')));
 }
 
-type PostProcessor = (s: any) => string;
-
-function postProcess(f: Outfile): Outfile {
-  const ext = f.path.match(/\.(.+)$/)![1];
-  if (ext && ext in postProcessors) {
-    const fn = postProcessors[ext] ?? (s => s);
-    f.content = fn(f.content);
-  }
-  return f;
-}
-
 interface Outfile {
   path: string;
   content: string | Buffer;
 }
 
-type ProcFn = (file: File, captureGroups: Record<string, string>) => Outfile | Outfile[];
-type Processor = [RegExp, ProcFn];
-
-const ProcessTsArrayFile: Processor = [/\/.*(?<slug>\[.+\]).*\.(?<ext>.+)\.js$/, (file, groups) => {
-  const array = file.module!.require().default as [string, string][];
-  return array.map(([slug, content]) => {
-    const filepath = file.path.replace(groups["slug"]!, slug);
-    return postProcess({ path: filepath.slice(0, -3), content });
-  });
-}];
-
-const ProcessTsFile: Processor = [/\.(?<ext>.+)\.js$/, (file, groups) => {
-  const content = file.module!.require().default;
-  return postProcess({ path: file.path.slice(0, -3), content });
-}];
-
-const ProcessAnyFile: Processor = [/./, (f) => f];
-
-const processors: Processor[] = [
-  ProcessTsArrayFile,
-  ProcessTsFile,
-  ProcessAnyFile,
-];
+const ARRAY_FILE = /\/.*(?<slug>\[.+\]).*\.(?<ext>.+)\.js$/;
+const FILE = /\.(?<ext>.+)\.js$/;
 
 export type SiteProcessor = (files: Iterable<File>) => Map<string, Buffer | string>;
 
@@ -61,15 +30,35 @@ export const processSite: SiteProcessor = (files) => {
   const outfiles = new Map<string, Buffer | string>();
 
   for (const file of files) {
-    const proc = processors.find(([r]) => file.path.match(r))!;
-    const [r, fn] = proc;
+    let specialFiles: Outfile[] | undefined;
 
-    const match = file.path.match(r)!;
-    const processed = fn(file, match.groups!);
-    const normalized = Array.isArray(processed) ? processed : [processed];
+    let match;
+    if (match = file.path.match(ARRAY_FILE)) {
+      const groups = match.groups!;
+      const exportedArray = file.module!.require().default as [string, string][];
+      specialFiles = exportedArray.map(([slug, content]) => {
+        const filepath = file.path.replace(groups["slug"]!, slug);
+        return { path: filepath, content };
+      });
+    }
+    else if (match = file.path.match(FILE)) {
+      const content = file.module!.require().default;
+      specialFiles = [{ path: file.path, content }];
+    }
 
-    for (const { path, content } of normalized) {
-      outfiles.set(path, content);
+    if (specialFiles) {
+      for (const file of specialFiles) {
+        file.path = file.path.slice(0, -3);
+
+        const ext = path.extname(file.path);
+        const fn = contentProcessors[ext];
+        if (fn) file.content = fn(file.content);
+
+        outfiles.set(file.path, file.content);
+      }
+    }
+    else {
+      outfiles.set(file.path, file.content);
     }
   }
 
