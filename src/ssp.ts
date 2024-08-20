9e5ce8b908dc5a8b323f52aa05209a44e2058dab
sdegutis <sbdegutis@gmail.com>
2024-08-20 08:06:34 -0500

    Start cleanup over overengineered solution.

--- src/ssp.ts
+++ src/ssp.ts
@@ -1,13 +1,11 @@
 import { File } from "./file.js";
 
-/** @deprecated */
-export const postProcessors: Record<string, PostProcessor> = {
+const postProcessors: Record<string, PostProcessor> = {
   html: hoistHtml,
   json: JSON.stringify,
 };
 
-/** @deprecated */
-export function hoistHtml(jsx: string) {
+function hoistHtml(jsx: string) {
   const hoisted = new Set<string>();
   return (jsx
     .replace(/<script .+?><\/script>|<link .+?>/g, (s, s2) => {
@@ -17,11 +15,9 @@ export function hoistHtml(jsx: string) {
     .replace(/<\/head>/, [...hoisted, '</head>'].join('')));
 }
 
-/** @deprecated */
-export type PostProcessor = (s: any) => string;
+type PostProcessor = (s: any) => string;
 
-/** @deprecated */
-export function postProcess(f: Outfile): Outfile {
+function postProcess(f: Outfile): Outfile {
   const ext = f.path.match(/\.(.+)$/)![1];
   if (ext && ext in postProcessors) {
     const fn = postProcessors[ext] ?? (s => s);
@@ -30,24 +26,15 @@ export function postProcess(f: Outfile): Outfile {
   return f;
 }
 
-/** @deprecated */
-export interface Outfile {
+interface Outfile {
   path: string;
   content: string | Buffer;
 }
 
-/** @deprecated */
-export type ProcFn = (file: File, captureGroups: Record<string, string>) => Outfile | Outfile[];
-/** @deprecated */
-export type Processor = [RegExp, ProcFn];
+type ProcFn = (file: File, captureGroups: Record<string, string>) => Outfile | Outfile[];
+type Processor = [RegExp, ProcFn];
 
-/** @deprecated */
-export const skip: ProcFn = () => [];
-/** @deprecated */
-export const asIs: ProcFn = (f) => f;
-
-/** @deprecated */
-export const ProcessTsArrayFile: Processor = [/\/.*(?<slug>\[.+\]).*\.(?<ext>.+)\.js$/, (file, groups) => {
+const ProcessTsArrayFile: Processor = [/\/.*(?<slug>\[.+\]).*\.(?<ext>.+)\.js$/, (file, groups) => {
   const array = file.module!.require().default as [string, string][];
   return array.map(([slug, content]) => {
     const filepath = file.path.replace(groups["slug"]!, slug);
@@ -55,26 +42,22 @@ export const ProcessTsArrayFile: Processor = [/\/.*(?<slug>\[.+\]).*\.(?<ext>.+)
   });
 }];
 
-/** @deprecated */
-export const ProcessTsFile: Processor = [/\.(?<ext>.+)\.js$/, (file, groups) => {
+const ProcessTsFile: Processor = [/\.(?<ext>.+)\.js$/, (file, groups) => {
   const content = file.module!.require().default;
   return postProcess({ path: file.path.slice(0, -3), content });
 }];
 
-/** @deprecated */
-export const ProcessAnyFile: Processor = [/./, asIs];
+const ProcessAnyFile: Processor = [/./, (f) => f];
 
-/** @deprecated */
-export const defaultProcessors: Processor[] = [
+const processors: Processor[] = [
   ProcessTsArrayFile,
   ProcessTsFile,
   ProcessAnyFile,
 ];
 
-/** @deprecated */
-export type SiteProcessor = (files: Iterable<File>, processors?: Processor[]) => Map<string, Buffer | string>;
+export type SiteProcessor = (files: Iterable<File>) => Map<string, Buffer | string>;
 
-export const processSite: SiteProcessor = (files, processors = defaultProcessors) => {
+export const processSite: SiteProcessor = (files) => {
   const outfiles = new Map<string, Buffer | string>();
 
   for (const file of files) {
