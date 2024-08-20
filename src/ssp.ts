11f273f74d9ebeee9413895e3446b25fdbeeaa0a
sdegutis <sbdegutis@gmail.com>
2024-08-20 10:59:20 -0500

    Fix name.

--- src/ssp.ts
+++ src/ssp.ts
@@ -1,5 +1,3 @@
-import { File } from "./file.js";
-
 export const contentProcessors: Record<string, (s: any) => string> = {
   '.html': hoistHtml,
   '.json': JSON.stringify,
@@ -18,7 +16,7 @@ function hoistHtml(jsx: string) {
 const isArrayFile = matcher<'ext' | 'slug'>(/\/.*(?<slug>\[.+\]).*(?<ext>\..+)\.js$/);
 const isSingleFile = matcher<'ext'>(/(?<ext>\..+)\.js$/);
 
-export type SiteProcessor = (files: Iterable<File>) => Map<string, Buffer | string>;
+export type SiteProcessor = (files: Iterable<FsFile>) => Map<string, Buffer | string>;
 
 export const processSite: SiteProcessor = (files) => {
   const outfiles = new Map<string, Buffer | string>();
