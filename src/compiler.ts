b8420b55e3a21edc4af9f38456d890ce784986d7
sdegutis <sbdegutis@gmail.com>
2024-08-28 09:22:52 -0500

    Simpler conditional plugin.

--- src/compiler.ts
+++ src/compiler.ts
@@ -10,8 +10,11 @@ export class Compiler {
     const plugins: babel.PluginItem[] = [
       [require('@babel/plugin-transform-typescript'), { isTSX: true }],
       babelPluginVanillaJSX,
-      this.#makeImportRenamer(!!browserFilePath),
     ];
+
+    if (browserFilePath) {
+      plugins.push(this.#importRenamer);
+    }
 
     if (realFilePath) {
       plugins.unshift(require('@babel/plugin-transform-modules-commonjs'));
@@ -25,37 +28,33 @@ export class Compiler {
     };
   }
 
-  #makeImportRenamer(inBrowser: boolean): babel.PluginItem {
-    return {
-      visitor: {
-        ImportDeclaration: {
-          enter: (path) => {
-            const dep = path.node.source.value;
-            if (inBrowser) {
-              const version: string | undefined = (
-                this.packageJson.devDependencies[dep] ??
-                this.packageJson.dependencies[dep]
-              );
-              let m;
-              if (m = version?.match(/^github:(?<user>.+?)\/(?<repo>.+?)(?:#(?<ver>.+))?$/)) {
-                const ver = m.groups!['ver'] ? `@${m.groups!['ver']}` : '';
-                path.node.source.value = `https://cdn.jsdelivr.net/gh/${m.groups!['user']}/${m.groups!['repo']}${ver}/index.js`;
-                console.log(path.node.source.value)
-              }
-              else if (version) {
-                path.node.source.value = `https://cdn.jsdelivr.net/npm/${dep}@${version}/+esm`;
-              }
-              else {
-                const typeDep = '@types/' + dep.replace(/^@(.+?)\/(.+)/, '$1__$2');
-                if (this.packageJson.devDependencies[typeDep]) {
-                  path.node.source.value = `https://cdn.jsdelivr.net/npm/${dep}/+esm`;
-                }
-              }
+  #importRenamer: babel.PluginItem = {
+    visitor: {
+      ImportDeclaration: {
+        enter: (path) => {
+          const dep = path.node.source.value;
+          const version: string | undefined = (
+            this.packageJson.devDependencies[dep] ??
+            this.packageJson.dependencies[dep]
+          );
+          let m;
+          if (m = version?.match(/^github:(?<user>.+?)\/(?<repo>.+?)(?:#(?<ver>.+))?$/)) {
+            const ver = m.groups!['ver'] ? `@${m.groups!['ver']}` : '';
+            path.node.source.value = `https://cdn.jsdelivr.net/gh/${m.groups!['user']}/${m.groups!['repo']}${ver}/index.js`;
+            console.log(path.node.source.value)
+          }
+          else if (version) {
+            path.node.source.value = `https://cdn.jsdelivr.net/npm/${dep}@${version}/+esm`;
+          }
+          else {
+            const typeDep = '@types/' + dep.replace(/^@(.+?)\/(.+)/, '$1__$2');
+            if (this.packageJson.devDependencies[typeDep]) {
+              path.node.source.value = `https://cdn.jsdelivr.net/npm/${dep}/+esm`;
             }
           }
         }
       }
-    };
-  }
+    }
+  };
 
 }
