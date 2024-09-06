05e67bdf2c0b1625f8126b1d4e6f36f1b3245c04
sdegutis <sbdegutis@gmail.com>
2024-09-06 08:11:08 -0500

    Export plugin.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 export { startDevServer } from './dev-server.js';
-export { File } from './file.js';
 export { generateFiles } from './file-generator.js';
+export { File } from './file.js';
 export { Module } from './module.js';
 export { Runtime } from './runtime.js';
 export * from './ssp.js';
+export { babelPluginVanillaJSX } from './vanillajsx.js';
 
